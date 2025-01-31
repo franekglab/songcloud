01970f51d11d724c5bf9d12d91b4c7f571a148fb
FranekGlab <franciszekglab695@gmail.com>
2025-01-31 02:49:51 +0100

    first commit after generating

---
+++ src/main/java/com/songcloud/SongcloudApplication.java
@@ -0,0 +1,13 @@
+package com.songcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songcloud/SongcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
